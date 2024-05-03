57b0f4b43196593515e3e0be9bfc42f807368da1
Pedro Brum <63755429+pedrocdasbrum@users.noreply.github.com>
2024-05-03 03:10:03 -0300

    Create busca_sequencial001.py

---
+++ programa_busca_sequencial/busca_sequencial/busca_sequencial001.py
@@ -0,0 +1,40 @@
+"""
+Programa busca_sequencial.py
+Descrição: Este programa busca um valor em uma base de dados
+Autor: Pedro Brum
+Versão: 0.0.1
+Data: 19/04/2024
+"""
+
+# Alocação de memória
+
+lista = []
+
+achou = False
+
+posicao = 0
+
+# Leitura da base de dados de cpf
+
+base = [1, 5, 2, 87, 31]
+
+lista = base
+
+# Leitura de dados
+
+cpf = int(input("\nDigite o valor a procurar: "))
+
+# Processamento de dados
+
+while posicao < len(lista):
+    if lista[posicao] == cpf:
+        achou = True
+        break
+    posicao += 1 # é a mesma coisa que posicao + 1
+
+# Saída de dados
+
+if achou:
+    print(f"\nO valor {cpf} foi encontrado na posição {posicao}")
+else:
+    print(f"\nO valor {cpf} não foi encontrado!")
